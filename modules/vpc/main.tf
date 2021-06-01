460d3c699cbd0275a96a54b787d6a4e361c52b48
kumar shanu <kshanu0602@gmail.com>
2021-06-02 01:43:35 +0530

    changes source and version

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,8 +14,10 @@
 
 
 module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "0.6.0"
+  #source  = "terraform-google-modules/network/google"
+  #version = "0.6.0"
+  source  = "terraform-google-modules/network/google//modules/vpc"
+  version = "~> 2.0.0"
 
   project_id   = "${var.project}"
   network_name = "${var.env}"
